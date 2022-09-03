1aae86c8929bd25b2e7fde17c6c6f9fbc248afc3
malikulle <malikulle@hotmail.com>
2022-09-03 22:24:17 +0300

    Validators

---
+++ src/demoProjects/Kodloma.io.Devs/Application/Features/ProgrammingLanguage/Commands/CreateProgrammingLanguage/CreateProgrammingLanguageValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.ProgrammingLanguage.Commands.CreateProgrammingLanguage
+{
+    public class CreateProgrammingLanguageValidator : AbstractValidator<CreateProgrammingLanguageCommand>
+    {
+        public CreateProgrammingLanguageValidator()
+        {
+            this.RuleFor(x => x.Name).NotEmpty();
+        }
+    }
+}
